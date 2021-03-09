0434766b1a7bdb0823a046526a14b9e464fd2626
Yu Mitsuhori <ymbullseye@gmail.com>
2021-03-09 21:22:19 +0900

    add flavor

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import {
   ScrollView,
   View,
   Text,
+  TextInput,
   StatusBar,
   Switch,
   TouchableOpacity,
@@ -40,29 +41,50 @@ const App: () => React$Node = () => {
           <RateForm />
           <TasteSlider />
           <BodySlider />
+          <Flavor />
         </View>
       </SafeAreaView>
     </>
   )
 }
 
+const Flavor = () => {
+  const [input, setInput] = useState('')
+  const onTextInput = useCallback((text: string) => {
+    setInput(text)
+  }, [])
+  return (
+    <View style={styles.formContainer}>
+      <Text style={styles.formTitle}>フレーバー</Text>
+      <TextInput
+        style={styles.flavorTextInput}
+        onChangeText={onTextInput}
+        value={input}
+      />
+    </View>
+  )
+}
+
 const TasteSlider = () => {
   return (
-    <View style={styles.flavorSlider}>
-      <View style={styles.sliderText}>
-        <Text>酸味</Text>
-      </View>
-      <Slider
-        style={{width: "75%", height: 40}}
-        minimumValue={0}
-        maximumValue={1}
-        thumbTintColor="#000"
-        minimumTrackTintColor="#000000"
-        maximumTrackTintColor="#000000"
-        step={0.1}
-      />
-      <View style={styles.sliderText}>
-        <Text>苦味</Text>
+    <View style={styles.formContainer}>
+      <Text style={styles.formTitle}>テイストバランス</Text>
+      <View style={styles.flavorSlider}>
+        <View style={styles.sliderTextView}>
+          <Text style={styles.sliderText}>酸味</Text>
+        </View>
+        <Slider
+          style={{width: "75%", height: 40}}
+          minimumValue={0}
+          maximumValue={1}
+          thumbTintColor="#000"
+          minimumTrackTintColor="#000000"
+          maximumTrackTintColor="#000000"
+          step={0.1}
+        />
+        <View style={styles.sliderTextView}>
+          <Text style={styles.sliderText}>苦味</Text>
+        </View>
       </View>
     </View>
   )
@@ -70,21 +92,24 @@ const TasteSlider = () => {
 
 const BodySlider = () => {
   return (
-    <View style={styles.flavorSlider}>
-      <View style={styles.sliderText}>
-        <Text>ライト</Text>
-      </View>
-      <Slider
-        style={{width: "75%", height: 40}}
-        minimumValue={0}
-        maximumValue={1}
-        thumbTintColor="#000"
-        minimumTrackTintColor="#000000"
-        maximumTrackTintColor="#000000"
-        step={0.1}
-      />
-      <View style={styles.sliderText}>
-        <Text>フル</Text>
+    <View style={styles.formContainer}>
+      <Text style={styles.formTitle}>ボディ</Text>
+      <View style={styles.flavorSlider}>
+        <View style={styles.sliderTextView}>
+          <Text style={styles.sliderText}>ライト</Text>
+        </View>
+        <Slider
+          style={{width: "75%", height: 40}}
+          minimumValue={0}
+          maximumValue={1}
+          thumbTintColor="#000"
+          minimumTrackTintColor="#000000"
+          maximumTrackTintColor="#000000"
+          step={0.1}
+        />
+        <View style={styles.sliderTextView}>
+          <Text style={styles.sliderText}>フル</Text>
+        </View>
       </View>
     </View>
   )
@@ -97,13 +122,16 @@ const RateForm = () => {
       setRate(enabled ? rate : rate - 1)
     }, [])
   return (
-    <View style={styles.rateForm}>
-      <RateStar id={1} currentRate={rate} onPressed={onPressed}/>
-      <RateStar id={2} currentRate={rate} onPressed={onPressed}/>
-      <RateStar id={3} currentRate={rate} onPressed={onPressed}/>
-      <RateStar id={4} currentRate={rate} onPressed={onPressed}/>
-      <RateStar id={5} currentRate={rate} onPressed={onPressed}/>
-     </View>
+    <View style={styles.formContainer}>
+      <Text style={styles.formTitle}>全体評価</Text>
+      <View style={styles.rateForm}>
+        <RateStar id={1} currentRate={rate} onPressed={onPressed}/>
+        <RateStar id={2} currentRate={rate} onPressed={onPressed}/>
+        <RateStar id={3} currentRate={rate} onPressed={onPressed}/>
+        <RateStar id={4} currentRate={rate} onPressed={onPressed}/>
+        <RateStar id={5} currentRate={rate} onPressed={onPressed}/>
+      </View>
+    </View>
   )
 }
 
@@ -126,7 +154,7 @@ const RateStar = (props: RateStarProps) => {
         <Icon
           name="star"
           color={enabled ? '#000' : '#ccc'}
-          size={50}
+          size={42}
         />
       </TouchableOpacity>
     </View>
@@ -136,6 +164,12 @@ const RateStar = (props: RateStarProps) => {
 const styles = StyleSheet.create({
   container: {
     margin: 12,
+  },
+  formContainer: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-start",
+    marginBottom: 16,
   },
   rateForm: {
     display: "flex",
@@ -147,14 +181,28 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-around",
     alignItems: "center",
-    marginTop: 12,
   },
-  sliderText: {
+  sliderTextView: {
     width: 50,
     display: "flex",
     flexDirection: "row",
-    justifyContent: "center"
-  }
+    justifyContent: "center",
+  },
+  slideText: {
+    fontSize: 12,
+  },
+  formTitle: {
+    marginBottom: 4,
+    fontSize: 15,
+    fontWeight: "bold",
+  },
+  flavorTextInput: {
+    width: "100%",
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 0,
+    borderBottomWidth: 1,
+  },
 })
 
 export default App
